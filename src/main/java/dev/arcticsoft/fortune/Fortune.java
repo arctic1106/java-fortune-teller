e05585e104b29f62c72e1a0aa9778fde2702f054
arctic1106 <arcticsoft@outlook.com>
2025-07-03 19:32:55 +0200

    update

--- src/main/java/dev/arcticsoft/fortune/Fortune.java
+++ src/main/java/dev/arcticsoft/fortune/Fortune.java
@@ -6,32 +6,32 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Fortune {
-	private final ArrayList<String> fortunes = new ArrayList<>();
+    private final ArrayList<String> fortunes = new ArrayList<>();
 
-	public Fortune() throws IOException {
-		loadJsonFortunes();
-	}
+    public Fortune() throws IOException {
+        loadJsonFortunes();
+    }
 
-	private void loadJsonFortunes() throws IOException {
-		var inputStream = getClass().getClassLoader().getResourceAsStream("fortunes.json");
-		new ObjectMapper().readTree(inputStream).get("data").elements()
-				.forEachRemaining(node -> fortunes.add(node.get("quote").asText()));
-	}
+    private void loadJsonFortunes() throws IOException {
+        var inputStream = getClass().getClassLoader().getResourceAsStream("fortunes.json");
+        new ObjectMapper().readTree(inputStream).get("data").elements()
+                .forEachRemaining(node -> fortunes.add(node.get("quote").asText()));
+    }
 
-	public void printRandomFortune() throws InterruptedException {
-		printWithTickerEffect(pickRandomFortune(), 100);
-	}
+    public void printRandomFortune() throws InterruptedException {
+        printWithTickerEffect(pickRandomFortune(), 100);
+    }
 
-	private String pickRandomFortune() {
-		return fortunes.get(new Random().nextInt(fortunes.size()));
-	}
+    private String pickRandomFortune() {
+        return fortunes.get(new Random().nextInt(fortunes.size()));
+    }
 
-	private void printWithTickerEffect(String fortune, int delayMillis) throws InterruptedException {
-		for (var c : fortune.toCharArray()) {
-			System.out.print(c);
-			System.out.flush();
-			Thread.sleep(delayMillis);
-		}
-		System.out.println();
-	}
+    private void printWithTickerEffect(String fortune, int delayMillis) throws InterruptedException {
+        for (var c : fortune.toCharArray()) {
+            System.out.print(c);
+            System.out.flush();
+            Thread.sleep(delayMillis);
+        }
+        System.out.println();
+    }
 }
--- src/main/java/dev/arcticsoft/fortune/Main.java
+++ src/main/java/dev/arcticsoft/fortune/Main.java
@@ -3,7 +3,8 @@ package dev.arcticsoft.fortune;
 import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) throws IOException, InterruptedException {
-		new Fortune().printRandomFortune();
-	}
+    
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new Fortune().printRandomFortune();
+    }
 }
